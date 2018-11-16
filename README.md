7fa905e88471f8cb6fe0fe2013e6510f432790f9
Jacob Anabi <anabi@chapman.edu>
2018-11-16 12:16:56 -0800

    works, for the most part

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # numbers
+
+*@author Jacob Anabi
+@date 2018-10-30
+@version 1.0
+2294644
+CPSC 298-nix*
+
 Contains bash script numbers.sh
--- numbers.sh
+++ numbers.sh
@@ -17,4 +17,10 @@ N=$1
 
 while [[ N -le pos_num  ]]
 do
-      
+	if [ $((N%2)) -eq 0 ]; then
+		echo $N Even
+	else
+		echo $N Odd
+	fi
+	N=$[$N+1]
+done
